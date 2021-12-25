b6f9166794bdb7e8a6808abd710df0aa5718fc57
Eugene <supernovaprotocol@gmail.com>
2021-12-25 12:00:10 +0700

    Enable storing N-gram based models in file

--- train_ngram.py
+++ train_ngram.py
@@ -1,22 +1,26 @@
 import math
+import os
+from collections import Counter, defaultdict
+import shelve
+
+from nltk import ngrams
+import numpy as np
+
 from datasets import NltkDataset
 from preprocessing import Paragraph, Encoder, build_vocab, tokenize_prompt, Sentence
-from nltk import ngrams
-from collections import Counter, defaultdict
-import numpy as np
 
 
 class NGramModel:
-    def __init__(self, count_tables, smoothing=True):
-        self.count_tables = count_tables
-        self.n = len(count_tables)
+    def __init__(self, counts_ensemble, smoothing=True):
+        self.counts_ensemble = counts_ensemble
+        self.n = self.counts_ensemble.n
         self.smoothing = smoothing
 
     def probabilities(self, tokens):
         return (self.p_next(prefix) for *prefix, _ in ngrams(tokens, self.n))
 
     def log_probability(self, tokens):
-        eps = 10**(-30)
+        eps = 10**(-10)
         return sum(math.log(self.p_next(prefix)[token] + eps)
                    for *prefix, token in ngrams(tokens, self.n))
 
@@ -53,7 +57,7 @@ class NGramModel:
 
     def p_next(self, prev_tokens):
         # todo: maybe add smoothing
-        counts_row = self._find_counts_row(prev_tokens)
+        counts_row = self.counts_ensemble.get_counts_row(prev_tokens)
 
         if self.smoothing:
             vocab_size = counts_row.shape[0]
@@ -61,23 +65,6 @@ class NGramModel:
             p = (counts_row + k) / (k * vocab_size + counts_row.sum())
             return p
         return counts_row / counts_row.sum()
-
-    def _find_counts_row(self, prev_tokens):
-        n = self.n
-        prev_tokens = tuple(prev_tokens)
-        while n > 0:
-            count_table = self.count_tables[n - 1]
-            prefix = prev_tokens[-n:]
-            try:
-                return count_table[prefix]
-            except KeyError:
-                n -= 1
-
-        unigram_table = self.count_tables[0]
-
-        assert len(unigram_table) == 1
-
-        return next(iter(unigram_table.values()))
 
     def generate(self, prompt, size):
         prefix_size = self.n - 1
@@ -115,29 +102,65 @@ def padded_ngram_tokens(fragments, n):
         yield from remainder
 
 
-def build_counts_table(classes, num_classes, n):
+def build_counts_table(classes, num_classes, n, save_path):
     if n == 0:
         raise ValueError(f'Wrong ngram order: {n}')
-    counts = Counter(ngrams(classes, n))
+    #counts = Counter(ngrams(classes, n))
 
-    count_table = defaultdict(lambda: np.zeros(num_classes, dtype=np.int32))
+    with shelve.open(save_path) as count_table:
+        for *n_1_gram, count_class in ngrams(classes, n):
+            key = '_'.join(map(str, n_1_gram))
+            if key not in count_table:
+                count_table[key] = np.zeros(num_classes, dtype=np.int32)
 
-    for n_gram, count in counts.items():
-        *n_1_gram, count_class = n_gram
-        counts_row = count_table[tuple(n_1_gram)]
-        counts_row[count_class] += count
-
-    # copying table into an ordinary dict that raises KeyError when accessing missing keys
-    return dict(count_table)
+            counts_row = count_table[key]
+            counts_row[count_class] += 1
+            count_table[key] = counts_row
+    print('Done', n)
 
 
-def build_ensemble_counts(train_fragments, encoder, n):
-    num_classes = len(encoder)
+class CountTableEnsemble:
+    @classmethod
+    def build_counts(cls, train_fragments, encoder, n, save_dir):
+        num_classes = len(encoder)
 
-    def classes_gen():
-        return encoded_classes(train_fragments, encoder, n)
+        def classes_gen():
+            return encoded_classes(train_fragments, encoder, n)
 
-    return [build_counts_table(classes_gen(), num_classes, n=i + 1) for i in range(n)]
+        os.makedirs(save_dir, exist_ok=True)
+        for i in range(n):
+            file_name = f'table_{i + 1}'
+            save_path = os.path.join(save_dir, file_name)
+            build_counts_table(classes_gen(), num_classes,
+                               n=i + 1, save_path=save_path)
+
+    def __init__(self, dir_path):
+        shelve_paths = [os.path.join(dir_path, f) for f in os.listdir(dir_path)]
+        shelve_paths.sort(key=lambda name: int(name.split('_')[-1]))
+        self.count_tables = [shelve.open(f) for f in shelve_paths if os.path.isfile(f)]
+        self.n = len(self.count_tables)
+
+    def get_counts_row(self, prev_tokens):
+        n = self.n
+
+        while n > 0:
+            count_table = self.count_tables[n - 1]
+            prefix = '_'.join(map(str, prev_tokens[-n:]))
+
+            try:
+                return count_table[prefix]
+            except KeyError:
+                n -= 1
+
+        unigram_table = self.count_tables[0]
+
+        assert len(unigram_table) == 1
+
+        return next(iter(unigram_table.values()))
+
+    def close(self):
+        for t in self.count_tables:
+            t.close()
 
 
 def encoded_classes(fragments, encoder, n):
@@ -149,13 +172,16 @@ train_fragments = ds.get_training_fragments()
 val_fragments = ds.get_validation_fragments()
 test_fragments = ds.get_test_fragments()
 
-ngram_order = 2
+ngram_order = 4
 
-vocab = build_vocab(padded_ngram_tokens(train_fragments, ngram_order), max_size=5000)
+vocab = build_vocab(padded_ngram_tokens(train_fragments, ngram_order), max_size=10000)
 encoder = Encoder.build(vocab)
 print('vocab size', len(vocab))
-count_tables = build_ensemble_counts(train_fragments, encoder, n=ngram_order)
-model = NGramModel(count_tables, smoothing=True)
+save_dir = 'counts_ensemble'
+CountTableEnsemble.build_counts(train_fragments, encoder, n=ngram_order, save_dir=save_dir)
+count_tables = CountTableEnsemble(save_dir)
+
+model = NGramModel(count_tables, smoothing=False)
 
 training = encoded_classes(train_fragments, encoder, ngram_order)
 validation = encoded_classes(val_fragments, encoder, ngram_order)
@@ -168,7 +194,7 @@ def calculate_perplexity(model, tokens):
     return np.exp(- logp / n)
 
 
-train_pp = calculate_perplexity(model, list(training))
+train_pp = calculate_perplexity(model, list(training)[:1000])
 val_pp = calculate_perplexity(model, list(validation)[:1000])
 print(train_pp)
 print(val_pp)
@@ -188,3 +214,7 @@ while True:
     print('Prompt:', prompt)
     print('Generated:', res)
 
+
+# todo: separate training from evaluation and sampling
+# todo: faster algorithm for building count tables (do not scan the whole dataset multiple times, batch write)
+# todo: implement effective smoothing techniques
